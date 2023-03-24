64753ffc14704397637737b805cbe990b26fe940
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-24 19:40:01 -0300

    UseContext

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import { TaskProvider } from '@/context/TasksContext'
 
 export const metadata = {
   title: 'ToDoList App by Rosario',
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <TaskProvider>{children}</TaskProvider>
+      </body>
     </html>
   )
 }
--- src/context/TasksContext.js
+++ src/context/TasksContext.js
@@ -1,8 +1,23 @@
-import { createContext } from 'react'
+'use client'
+import { createContext, useContext } from 'react'
 
-const TaskContext = createContext()
+export const TaskContext = createContext()
+
+export const useTasks = () => {
+  const context = useContext(TaskContext)
+  if (!context) throw new Error('useTasks must used within a provider')
+  return context
+}
 
 export const TaskProvider = ({ children }) => {
   const tasks = []
-  return <TaskContext.Provider value={tasks}>{children}</TaskContext.Provider>
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+      }}
+    >
+      {children}
+    </TaskContext.Provider>
+  )
 }
