e442940027dc82ee8ebc76b26e408d883945457f
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-24 20:13:13 -0300

    Create task

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+import { useState } from 'react'
 
 const Page = () => {
+  const [task, setTask] = useState()
+
+  const handleChange = (e) =>
+    setTask({ ...task, [e.target.name]: e.target.value })
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(task)
+    }
   return (
-    <div>Task Form</div>
+    <form onSubmit={handleSubmit}>
+      <input name="title" placeholder="Write a title" onChange={handleChange} />
+      <textarea
+        name="description"
+        placeholder="Write a description"
+        onChange={handleChange}
+      />
+      <button>Save</button>
+    </form>
   )
 }
 
--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import {useRouter} from "next/navigation"
+import { useRouter } from 'next/navigation'
 
 const TaskCard = ({ task }) => {
-    const router = useRouter()
+  const router = useRouter()
   return (
-    <div onClick={() => {
-router.push(`/edit/${task.id}`)
-    }}>
+    <div
+      onClick={() => {
+        router.push(`/edit/${task.id}`)
+      }}
+    >
       <h1>{task.title}</h1>
       <button>Delete</button>
       <p>{task.description}</p>
