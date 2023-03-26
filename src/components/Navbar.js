1a3312bd93e74e318a313d2e41670ea2cc99af37
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-26 17:18:36 -0300

    Added task counter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 "use client"
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
+import { useTasks } from '@/context/TasksContext'
 
 export const Navbar = () => {
   const router = useRouter()
+  const { tasks } = useTasks()
   return (
     <header className='flex justify-between items-center bg-gray-800 px-28 py-3'>
       <Link href="/">
-        <h1 className='font-bold text-3xl text-white'>Task app</h1>
+        <h1 className='font-bold text-3xl text-white'>Task app
+        <span className='text-slate-300 text-sm ml-5'>{tasks.length} tasks</span>
+        </h1>
       </Link>
 
       <div>
