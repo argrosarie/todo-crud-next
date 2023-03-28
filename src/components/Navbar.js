17158cb50b119e50ad03e487284fd19eb7301e72
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-27 21:01:07 -0300

    Refactor some header styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { useTasks } from '@/context/TasksContext'
@@ -7,17 +7,20 @@ export const Navbar = () => {
   const router = useRouter()
   const { tasks } = useTasks()
   return (
-    <header className='flex justify-between items-center bg-gray-800 px-28 py-3'>
-      <Link href="/">
-        <h1 className='font-bold text-3xl text-white'>El Tasker
-        <span className='text-slate-300 text-sm ml-5'>{tasks.length} tasks</span>
-        </h1>
-      </Link>
-
-      <div>
-        <button onClick={() => router.push('/new')}
-        className="bg-green-500 hover:bg-green-400 px-5 py-2 text-gray-50 font-bold rounded-sm inline-flex items-center"
-        >Add task</button>
+    <header className="flex justify-between items-center bg-gray-800">
+      <div className="flex flex-col my-2 ml-5">
+        <Link href="/">
+          <h1 className="font-bold text-3xl text-white">El Tasker</h1>
+        </Link>
+        <span className="text-slate-300 text-sm">{tasks.length} tasks</span>
+      </div>
+      <div className='mr-5'>
+        <button
+          onClick={() => router.push('/new')}
+          className="bg-green-500 hover:bg-green-400  text-gray-50 font-bold rounded-sm flex items-center"
+        >
+          Add task
+        </button>
       </div>
     </header>
   )
