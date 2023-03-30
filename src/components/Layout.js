88c215e468b555280953414279214438afdecaf0
rosarie <rosariosanchezsampietro@gmail.com>
2023-03-30 08:11:33 -0300

    Fixed navbar margin

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 export const Layout = ({children}) => {
     return(
-        <div className="bg-gray-900 text-white h-full">
+        <div className="bg-gray-900 text-white h-full w-screen">
             <main className="h-screen px-28 py-10">{children}</main>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
   const router = useRouter()
   const { tasks } = useTasks()
   return (
-    <header className="flex justify-between items-center bg-gray-800">
+    <header className="flex justify-between items-center bg-gray-800 w-screen">
       <div className="flex flex-col my-2 ml-5">
         <Link href="/">
           <h1 className="font-bold text-3xl text-white">El Tasker</h1>
@@ -17,7 +17,7 @@ export const Navbar = () => {
       <div className='mr-3 p-2'>
         <button
           onClick={() => router.push('/new')}
-          className="bg-green-500 hover:bg-green-400  text-gray-50 font-bold rounded-sm flex items-center p-2"
+          className="bg-green-500 hover:bg-green-400  text-gray-50 font-bold rounded-sm flex items-center p-2 md:mr-3"
         >
           Add task
         </button>
